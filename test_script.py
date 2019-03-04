7a66e30a23c1911292a91b5f27f36f775a3dde44
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-03-04 10:56:15 +0000

    I think everything works now

--- test_script.py
+++ test_script.py
@@ -2,7 +2,7 @@ from helmholtz_monte_carlo import error_analysis as err
 import pickle
 
 
-k_range = [30.0,40.0]
+k_range = [30.0,50.0,60.0]
 
 h_spec = (1.0,-1.5)
 
@@ -18,23 +18,26 @@ lambda_mult = 1.0
 
 qoi = 'integral'
 
-print('Quasi Monte Carlo')
-qmc_out = err.investigate_error(k_range,h_spec,J_range,nu,M_range,'qmc',delta,lambda_mult,qoi,dim=2)
+# I'm hacking things so that I get the results as they come
 
-print('qmc')
-print(qmc_out)
+for k in k_range:
 
-print('Monte Carlo')
-mc_out = err.investigate_error(k_range,h_spec,J_range,nu,M_range,'mc',delta,lambda_mult,qoi,dim=2)
+    print('Quasi Monte Carlo')
+    qmc_out = err.investigate_error([k],h_spec,J_range,nu,M_range,'qmc',delta,lambda_mult,qoi,dim=2)
 
-print('mc')
-print(mc_out)
+    print('qmc')
+    print(qmc_out)
+    with open(str(k)+'-qmc.pickle','wb') as f:
+        pickle.dump(qmc_out,f)
 
-with open('mc.pickle','wb') as f:
-    pickle.dump(mc_out,f)
+    print('Monte Carlo')
+    mc_out = err.investigate_error([k],h_spec,J_range,nu,M_range,'mc',delta,lambda_mult,qoi,dim=2)
 
-with open('qmc.pickle','wb') as f:
-    pickle.dump(qmc_out,f)
+    print('mc')
+    print(mc_out)
+
+    with open(str(k)+'-mc.pickle','wb') as f:
+        pickle.dump(mc_out,f)
 
 
 
