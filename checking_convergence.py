33687f40792894c4bc2ae3f212c1a8d97c55749a
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-03-22 15:11:08 +0000

    (Slow) script that checks FEM and QMC convergence

--- checking_convergence.py
+++ checking_convergence.py
@@ -4,15 +4,15 @@ import numpy as np
 
 # All the parameters for assesing convergence
 
-k = 10.0
+k_list =  [10.0,20.0,30.0]
 
-ppw = 10
+#ppw = 10
 
-h_coarse_spec = (2.0*np.pi/ppw,-1.0)
+h_coarse_spec = (1.0,-1.5)#(2.0*np.pi/ppw,-1.0)
 
 h_refinement_levels = 2
 
-h_fine_spec = (h_coarse_spec[0]/(2.0**h_refinement_levels),-1.0)
+h_fine_spec = (h_coarse_spec[0]/(2.0**h_refinement_levels),h_coarse_spec[1])
 
 # N = #QMC points = 2**M
 
@@ -34,28 +34,30 @@ lambda_mult = 1.0
 
 qois = ['integral','origin']
 
-# First fix h and vary number of QMC points
+# Actually run the code
+for k in k_list:
+    # First fix h and vary number of QMC points
 
-h_spec = h_fine_spec
+    h_spec = h_fine_spec
 
-for M in range(M_low,M_high+1):
+    for M in range(M_low,M_high+1):
 
-    qmc_out = err.investigate_error(k,h_spec,J,nu,M,'qmc',delta,lambda_mult,qois,dim=2,display_progress=True)
+        qmc_out = err.investigate_error(k,h_spec,J,nu,M,'qmc',delta,lambda_mult,qois,dim=2,display_progress=True)
 
-    print(qmc_out)
-    with open(str(k)+'-qmc-points-varying-qmc.pickle','wb') as f:
-        pickle.dump(qmc_out,f)
+        print(qmc_out)
+        with open(str(k)+'-qmc-points-varying-qmc.pickle','wb') as f:
+            pickle.dump(qmc_out,f)
 
-# Then fix number of QMC points and vary h
+    # Then fix number of QMC points and vary h
 
-for h_refinment in range(h_refinement_levels+1):
+    for h_refinement in range(h_refinement_levels+1):
 
-    h_spec = (h_coarse_spec[0]/(2.0**h_refinement),-1.0)
-    
-    qmc_out = err.investigate_error(k,h_spec,J,nu,M,'qmc',delta,lambda_mult,qois,dim=2,display_progress=True)
+        h_spec = (h_coarse_spec[0]/(2.0**h_refinement),-1.0)
 
-    print(qmc_out)
-    with open(str(k)+'-h-varying-qmc.pickle','wb') as f:
-        pickle.dump(qmc_out,f)
+        qmc_out = err.investigate_error(k,h_spec,J,nu,M,'qmc',delta,lambda_mult,qois,dim=2,display_progress=True)
+
+        print(qmc_out)
+        with open(str(k)+'-h-varying-qmc.pickle','wb') as f:
+            pickle.dump(qmc_out,f)
 
 
