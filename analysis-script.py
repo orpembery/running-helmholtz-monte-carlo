cabdabd1e3edb47566dae98a4ae153f9c9dd7392
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-04-04 07:52:26 +0100

    Line of best fit now plotted

--- analysis-script.py
+++ analysis-script.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     # Each entry of the list corresponds to a shift
     # And each is itself a list of length 2
     # Each entry of which corresponds to a different QoI
-    # And each is itself a list of length 1024, containing floats
+    # And each is itself a list of length (number of samples), containing floats
 
     num_qois = 2
 
@@ -54,7 +54,7 @@ if __name__ == "__main__":
 
     good_errors = []
 
-    qoi_num = 0 # 1 = point evaluation at the origin, 0 means integral (in current setup)
+    qoi_num =  int(sys.argv[4])# 1 = point evaluation at the origin, 0 means integral (in current setup)
 
     for M in range(M_low,M_large+1):
 
@@ -72,20 +72,20 @@ if __name__ == "__main__":
 
         good_errors.append(M_out[1][qoi_num])
         
-        print(M)
-        print(M_out)
+        #print(M)
+        #print(M_out)
             
     M_list = list(range(M_low,M_large+1))
 
-    print(M_list)
+    #print(M_list)
 
-    print(good_errors)
+    #print(good_errors)
     
-    print(len(M_list))
+    #print(len(M_list))
 
-    print(len(good_errors))
+    #print(len(good_errors))
 
-    print(list(range(M_large)))
+    #print(list(range(M_large)))
     
     p_approx = [ - np.log2(good_errors[ii]/good_errors[ii+1]) for ii in range(M_large-M_low)]
 
@@ -95,8 +95,44 @@ if __name__ == "__main__":
 
     N_list_inc_shifts = 20.0 * N_list # Get same convergence rate with this
     
-    plt.loglog(N_list,good_errors)
+    plt.loglog(N_list,good_errors,'x')
 
+    # Add line of best fit
+    # Am I using the right log?
+    
+    fit = np.polyfit(np.log10(N_list),np.log10(good_errors),deg=1)
+
+    #print(np.log(N_list))
+
+    #    print(np.log(good_errors))
+
+    print('Slope of line of best fit:')
+    print(fit[0])
+    
+    #print(fit)
+    
+    # entry 0 is intercept, 1 is gradient
+
+    # There must be an easy way to do this
+    
+    big_N = max(N_list)
+
+    small_N = min(N_list)
+    
+    x_for_fit = [small_N,big_N]
+
+    y_for_fit = [10.0**(fit[1]+fit[0]*np.log10(small_N)),10.0**(fit[1]+fit[0]*np.log10(big_N))]
+
+    #print(N_list)
+
+    #print(good_errors)
+    
+    #print(x_for_fit)
+
+    #print(y_for_fit)
+    
+    plt.loglog(x_for_fit,y_for_fit,'--')
+    
     plt.xlabel('log(N)')
 
     plt.ylabel('log(QMC Error)')
@@ -106,15 +142,17 @@ if __name__ == "__main__":
     elif qoi_num == 0:
         plt.title('QMC error for integral of solution for k = '+k)
 
-#    plt.plot([np.log(N_list[0]),np.log(N_list[-1])],[np.log(good_errors[0]),np.log(good_errors[0])-(np.log(N_list[-1])-np.log(N_list[0]))])
-    
+    #    plt.plot([np.log(N_list[0]),np.log(N_list[-1])],[np.log(good_errors[0]),np.log(good_errors[0])-(np.log(N_list[-1])-np.log(N_list[0]))]) # What is this?
+
+
+
     plt.show()
 
     
 
-    print(len(p_approx))
+    #print(len(p_approx))
     
-    print(p_approx)
+    #print(p_approx)
         
     # For each QoI:
 
