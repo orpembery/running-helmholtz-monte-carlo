75a2152408bcb8cdaea9b5e009cb011f1090ce4e
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-08-03 10:31:34 +0100

    Scripts for k = 10, 20, 30, scaling the number of QMC points and doing 20 shifts. To assess whether QMC error remains bounded

---
+++ running_scaling_qmc_points_with_shifts.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Yes, this will need to be made reproducible....
+
+for J in 10
+do
+    for delta in 1.0
+    do
+	for j_scaling in 4.0
+	do	    
+	    echo ${J}
+	    echo ${delta}
+	    echo ${j_scaling}
+	    sbatch jobscript10-var-points-shifts.slurm ${J} ${delta} ${j_scaling}
+            sbatch jobscript20-var-points-shifts.slurm ${J} ${delta} ${j_scaling}
+            sbatch jobscript30-var-points-shifts.slurm ${J} ${delta} ${j_scaling}
+            #sbatch jobscript40-GMRES.slurm ${J} ${delta} ${j_scaling}
+            #sbatch jobscript50-GMRES.slurm ${J} ${delta} ${j_scaling}
+            #sbatch jobscript60-GMRES.slurm ${J} ${delta} ${j_scaling}
+	done
+    done
+done
