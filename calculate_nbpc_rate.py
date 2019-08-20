18a791084d2d86dc32d731a9d18ff3116e0bdf32
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-08-20 14:12:32 +0100

    Slightly tweaked structure of fitting code, so a user can access the fit.

--- calculate_nbpc_rate.py
+++ calculate_nbpc_rate.py
@@ -1,27 +1,13 @@
-import numpy as np
-from matplotlib import pyplot as plt
 import sys
+from running_helmholtz_monte_carlo.nbpc_percentage_fit import nbpc_fit
 
 k = float(sys.argv[1])
 
-k_list = np.array([10.0,20.0,30.0])
+fits = nbpc_fit()
 
-prop  = [0.24,0.48,0.78]
+fit_0 = fits[0]
 
-#plt.plot(k_list,prop,'o')
-
-fit = np.polyfit(k_list,prop,deg=1)
-
-# Polyfit uses (in my opinion) the wrong notational convention
-fit_0 = fit[1]
-
-fit_1 = fit[0]
-
-#print(fit_0,fit_1)
-
-#plt.plot(k,fit_0 + fit_1*k,'--')
-
-#plt.show()
+fit_1 = fits[1]
 
 # The above figures were percentages, but we need a proportion
 
---
+++ running_helmholtz_monte_carlo/nbpc_percentage_fit.py
@@ -0,0 +1,31 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+# From the (inputted by hand) results of the sequential nbpc algorithm, compute the fit
+
+def nbpc_fit(printing=False):
+
+    k_list = np.array([10.0,20.0,30.0])
+
+    prop  = [0.24,0.48,0.78]
+
+    plt.plot(k_list,prop,'o')
+
+    fit = np.polyfit(k_list,prop,deg=1)
+
+    # Polyfit uses (in my opinion) the wrong notational convention
+    fit_0 = fit[1]
+
+    fit_1 = fit[0]
+
+    if printing:
+
+        print(fit_0,fit_1)
+
+    else:
+
+        return [fit_0,fit_1]
+   
+if __name__ == '__main__':
+
+    nbpc_fit(True)
