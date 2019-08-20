52432c06816523bf54aa8c94a0e835e494d40dbc
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-08-20 13:55:36 +0100

    Version of running script used for (what I hope) are the final version of the nbpc-qmc computations.

--- running_variable_M.sh
+++ running_variable_M.sh
@@ -11,11 +11,11 @@ do
 	    echo ${J}
 	    echo ${delta}
 	    echo ${j_scaling}
-	    #sbatch jobscript10-GMRES.slurm ${J} ${delta} ${j_scaling}
-            #sbatch jobscript20-GMRES.slurm ${J} ${delta} ${j_scaling}
-            #sbatch jobscript30-GMRES.slurm ${J} ${delta} ${j_scaling}
-            #sbatch jobscript40-GMRES.slurm ${J} ${delta} ${j_scaling}
-            #sbatch jobscript50-GMRES.slurm ${J} ${delta} ${j_scaling}
+	    sbatch jobscript10-GMRES.slurm ${J} ${delta} ${j_scaling}
+            sbatch jobscript20-GMRES.slurm ${J} ${delta} ${j_scaling}
+            sbatch jobscript30-GMRES.slurm ${J} ${delta} ${j_scaling}
+            sbatch jobscript40-GMRES.slurm ${J} ${delta} ${j_scaling}
+            sbatch jobscript50-GMRES.slurm ${J} ${delta} ${j_scaling}
             sbatch jobscript60-GMRES.slurm ${J} ${delta} ${j_scaling}
 	done
     done
