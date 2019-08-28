13dc1fe3dd25777dff0d2d2551cf3284f1db3230
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-08-28 21:05:14 +0200

    Modified calculating rate file.

--- calculate_nbpc_rate.py
+++ calculate_nbpc_rate.py
@@ -1,15 +1,23 @@
 import sys
 from running_helmholtz_monte_carlo.nbpc_percentage_fit import nbpc_fit
 
-k = float(sys.argv[1])
+def make_fits():
 
-fits = nbpc_fit()
+    fits = nbpc_fit()
 
-fit_0 = fits[0]
+    fit_0 = fits[0]
 
-fit_1 = fits[1]
+    fit_1 = fits[1]
+
+    return [fit_0,fit_1]
 
 # The above figures were percentages, but we need a proportion
 
-print((fit_0 + fit_1*k)/100.0)
+if __name__ == '__main__':
+
+    [fit_0,fit_1] = make_fits()
+
+    k = float(sys.argv[1])
+
+    print((fit_0 + fit_1*k)/100.0)
    
